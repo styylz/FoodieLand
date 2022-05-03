ccd545b239c691ac5648bd8bdf48dec4dbaf1850
Miroslav I <styylzeh@gmail.com>
2022-05-03 13:37:26 +0300

    feat: created categories and simple-card

--- src/components/index.ts
+++ src/components/index.ts
@@ -19,3 +19,6 @@ export * from './navbar/Navbar'
 
 //badges
 export * from './badges/Badge'
+
+//cards
+export * from './cards/SimpleCard'
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Navbar } from 'components';
 import Banner from './sections/Banner';
+import Categories from './sections/Categories';
 
 
 const Home: React.FC = () => {
@@ -8,6 +9,7 @@ const Home: React.FC = () => {
 		<Box>
 			<Navbar />
 			<Banner />
+			<Categories/>
 		</Box>
 	)
 };
